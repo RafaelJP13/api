03d89c6a9326050e42692f01231b323b41394ea2
Rafael Fernandes <Rafaelsf013@gmail.com>
2022-11-05 10:47:14 -0300

    #feat Inicio do middleware isAdmin

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -3,18 +3,17 @@ import users from '../data/users.js'
 const isAdmin = (req, res, next) => {
     
     const { callerId } = req.body
-    console.log(req.body)
 
-    // if(callerId && !isNaN(callerId)) 
+    if(callerId && !isNaN(callerId)) 
 
-    //     users.find(user => user.id === callerId)?.permissao === 'ADMIN' ? next() : res.send(401, "Não Autorizado!")
+        users.find(user => user.id === callerId)?.permissao === 'ADMIN' ? next() : res.send(401, "Não Autorizado!")
      
-    // else{
+    else{
 
-    //     res.send(400, 'O parâmetro callerId deve ser númerico!')
-    //     next()
+        res.send(400, 'O parâmetro callerId deve ser númerico!')
+        next()
 
-    // }
+    }
     next()
     
 }
