3f1350c184235c78ed6a0f2d5e5e8ddc399622fb
Rafael Fernandes <Rafaelsf013@gmail.com>
2022-11-05 11:42:10 -0300

    #fix Corrigir middleware

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -4,17 +4,7 @@ const isAdmin = (req, res, next) => {
     
     const { callerId } = req.body
 
-    if(callerId && !isNaN(callerId)) 
-
-        users.find(user => user.id === callerId)?.permissao === 'ADMIN' ? next() : res.send(401, "Não Autorizado!")
-     
-    else{
-
-        res.send(400, 'O parâmetro callerId deve ser númerico!')
-        next()
-
-    }
-    next()
+    callerId ?? users.find(user => user.id === callerId)?.permissao === 'ADMIN' ? next() : res.send(401, "Não Autorizado!")
     
 }
 
