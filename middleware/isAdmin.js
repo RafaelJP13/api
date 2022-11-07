4a54aff200f2c6c117a6c21e26f6dc520076bd0e
Rafael Fernandes <Rafaelsf013@gmail.com>
2022-11-07 10:06:21 -0300

    #fix Adicionar operador unary

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -4,13 +4,12 @@ const isAdmin = (req, res, next) => {
     
     const { callerId } = req.body
 
-    if(!callerId) res.status(404).json({msg:'Informe o parametro callerId!'})
-    if(isNaN(callerId)) res.status(404).json({msg:'O parametro callerId deve ser númerico!'})
-
-    const user = users.find(user => user.id == callerId)
+    if(!callerId) res.status(404).json({msg:'Informe o parâmetro callerId!'})
+    
+    const user = users.find(user => user.id === +callerId)
 
     if(!user) res.status(404).json({msg:'Não Autorizado!'})
-    
+
     user.permissao === 'ADMIN' ? next() : res.status(401).json({msg:"Não Autorizado!"})
 
 }
