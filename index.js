c7462721671c06517d00895de2a721a09c9af3b3
Rafael Fernandes <Rafaelsf013@gmail.com>
2022-11-07 10:03:08 -0300

    #fix Remover comentários e ponto e virgula

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
 import express from 'express'
 import cors from 'cors'
-import isAdmin from './middleware/isAdmin.js'
 import RouteUsers from './routes/users.js'
 
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use(cors());
 
 app.use('/users', RouteUsers)
 
-
-// ROTAS EXECUTANDO FUNÇÕES CRUD NA ARRAY DE USUÁRIOS, ONDE SOMENTE O ADMINISTRADOR PODE CRIAR OU DELETAR UM USUÁRIO.
-// ENVIE A ID DE QUEM ESTÁ ENVIANDO O REQUEST COMO PARÂMETRO NA URL " calledId "
-// CRIE AS SEGUINTES ROTAS.
-
-//TODO
-// GET /users
-// POST /users
-// PATCH /users/:id
-// DELETE /users/:id
-
-
-app.listen(3000);
+app.listen(3000)
