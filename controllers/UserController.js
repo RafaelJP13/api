c1a26c2ed39f2693109d0c523453e8b83e9e9c21
Rafael Fernandes <Rafaelsf013@gmail.com>
2022-11-07 09:12:09 -0300

    #feat alterar nome da constante userId e Adicionar metodo de deletar

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,13 +4,13 @@ const getUsers = (req, res) => users.length > 0 ? res.status(200).json(users) : 
 
 const getUser = (req, res) => {
 
-    const {userId} = req.params
+    const {id} = req.params
 
-    if(!userId) res.status(404).json({msg:'Informe o parametro userId!'})
+    if(!id) res.status(404).json({msg:'Informe o parametro id!'})
 
     if(!users || users.length === 0) res.status(200).json({users:null})
 
-    const filteredUser = users.filter(user => user.id === +userId)
+    const filteredUser = users.filter(user => user.id === +id)
     filteredUser.length > 0 ? res.status(200).json({users:filteredUser}) : res.status(200).json({users: null})
 }
 
@@ -45,11 +45,35 @@ const createUser = (req, res) => {
 
 const updateUser = (req, res) => {
 
+    // const {id, nome, empresa, permissao, callerId} = req.body
+
+    // if(!id) res.status(404).json({msg:'Informe o parametro id!'})
+
+    // const searchedUser = users.find(user => id == user.id)
+
+    // if(searchedUser === undefined) res.status(404).json({msg:'Usuário não encontrado no sistema!'})
 
 }
 
 const deleteUser = (req, res) => {
 
+    const {callerId} = req.body
+    const {id} = req.params
+
+    if(!id) res.status(404).json({msg:'Informe o parametro id!'})
+    if(isNaN(id)) res.status(404).json({msg:'O parametro id deve ser númerico!'})
+    if(!callerId) res.status(404).json({msg:'Informe o parametro callerId!'})
+    if(callerId === id ) res.status(404).json({msg:'Não é possível excluir si mesmo!'})
+    
+    const searchedUser = users.find(user => id == user.id)
+
+    if(searchedUser === undefined) res.status(404).json({msg:'Usuário não encontrado no sistema!'})
+
+    const indexOfObject = users.findIndex(user => user.id == id)
+
+    if(indexOfObject >= 0) users.splice(indexOfObject, 1);
+      
+    res.status(200).json({msg: 'Usuário removido com sucesso!'})
 
 }
 
